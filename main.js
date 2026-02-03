d73ba4444dea5db002673e8baefbc84b7a054be8
Taras Shcherban <sherbantaras535@gmail.com>
2026-02-03 19:46:12 +0200

    Update

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const apiUrl = 'https://api!';
-const postData = { };
+const apiUrl = 'http://localhost:8080/getRequest';
+
+const postData = {
+    firstnumber: 50.6,
+    secondnumber: 12.4,
+    sign: "/"
+};
 
 fetch(apiUrl, {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     },
-    body: JSON.stringify(postData),
+    body: JSON.stringify(postData)
 })
-    .then(response => response.json())
-    .then(data => console.log('Success: ', data))
-    .catch(error => console.error('Error:', error));
+.then(response => {
+    if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+    }
+    return response.json();
+})
+.then(data => console.log('Success:', data))
+.catch(error => console.error('Error:', error));
