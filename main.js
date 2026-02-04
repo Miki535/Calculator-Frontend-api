8baae6bad294ab3e4d475ef59f086d93bc4bd243
Taras Shcherban <sherbantaras535@gmail.com>
2026-02-04 18:49:31 +0200

    Update

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-var firstNumber = document.getElementById('firstnumber');
-var secondNumber = document.getElementById('secondnumber');
-var sign = document.getElementById('sign');
 const apiUrl = 'http://localhost:8080/getRequest';
 
-const postData = {
-    firstnumber: firstNumber,
-    secondnumber: secondNumber,
-    sign: sign
-};
+function checkForm(el) {
+    var firstNumber = parseFloat(document.getElementById('firstnumber').value);
+    var secondNumber = parseFloat(document.getElementById('secondnumber').value);
+    var sign = document.getElementById('sign').value;
 
-fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(postData)
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('HTTP error ' + response.status);
-    }
-    return response.json();
-})
-.then(data => console.log('Success:', data))
-.catch(error => console.error('Error:', error));
+    console.log(firstNumber, secondNumber, sign);
+
+    const postData = {
+        firstnumber: firstNumber,
+        secondnumber: secondNumber,
+        sign: sign
+    };
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP error ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+
+    return false;
+}
