087c501459aee4ab0b60d96c1774d4c8fdc3e20f
Taras Shcherban <sherbantaras535@gmail.com>
2026-02-05 18:05:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-function checkForm(el) {
-    var firstNumber = parseFloat(document.getElementById('firstnumber').value);
-    var secondNumber = parseFloat(document.getElementById('secondnumber').value);
-    var sign = document.getElementById('sign').value;
+function checkForm() {
+    const firstNumber = parseFloat(document.getElementById('firstnumber').value);
+    const secondNumber = parseFloat(document.getElementById('secondnumber').value);
+    const sign = document.getElementById('sign').value;
 
     let apiUrl;
+    let postData;
 
-    if (sign != "+" && sign != "-" && sign != "*" && sign != "/") {
-        apiUrl = 'http://localhost:8080/getRequestMath';
-    }else {
+    const basicSigns = ["+", "-", "*", "/"];
+
+    if (basicSigns.includes(sign)) {
         apiUrl = 'http://localhost:8080/getRequestCalc';
-    }
-
-    console.log(firstNumber, secondNumber, sign);
-
-    const postData = {
-        firstnumber: firstNumber,
-        secondnumber: secondNumber,
-        sign: sign
+        postData = {
+            firstnumber: firstNumber,
+            secondnumber: secondNumber,
+            sign: sign
     };
-
-    const postDataMath = {
-        number: firstNumber,
-        secondNum: secondNumber,
-        type: sign
+    } else {
+        apiUrl = 'http://localhost:8080/getRequestMath';
+        postData = {
+            number: firstNumber,
+            secondnum: secondNumber,
+            type: sign
+        };
     }
 
     fetch(apiUrl, {
@@ -30,7 +29,7 @@ function checkForm(el) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(postDataMath)
+        body: JSON.stringify(postData)
     })
     .then(response => {
         if (!response.ok) {
@@ -41,15 +40,15 @@ function checkForm(el) {
     .then(data => {
         const output = document.getElementById("output");
 
-        if (data.result !== undefined && data.result !== null && data.result !== "") {
+        if (data.result) {
             output.style.display = "block";
             output.textContent = "Result: " + data.result;
         } else {
             output.style.display = "none";
         }
+
         console.log("Success:", data);
     })
-    .then(data => console.log('Success:', data))
     .catch(error => console.error('Error:', error));
 
     return false;
